67f9157b3676ece0868286b9bd507228f8940f73
Kaan <kkureli@gmail.com>
2020-01-28 02:03:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return "This Saturday, I want to " + activity + "!"
+}
+
+function mondayWork(activity = 'go to the office') {
+    return 'This Monday, I will ' + activity + '.'
+}
+
+function wrapAdjective(string = '*') {
+    return function inner1(params = 'special') {
+        return 'You are ' + string + params + string + '!'
+    }
+}
+
+let Calculator = {
+    add: function() {
+        return 1 + 3
+    },
+    subtract: function() {
+        return 1 - 3;
+    },
+    multiply: function() {
+        return 1 * 3
+    },
+    divide: function() {
+        return 10 / 5
+    }
+}
+
+function actionApplyer(start, arr) {
+    if (arr.length === 0) {
+        return start
+    } else {
+        return (start + 1) - 10
+    }
+}
