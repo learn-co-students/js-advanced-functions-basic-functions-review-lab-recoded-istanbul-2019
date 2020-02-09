e584ddc4ceef23932879962e0c97ff53c37dde30
mmelihdogan <doganmmelih@gmail.com>
2020-02-10 01:02:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function(hmm = "special") { // The "inner" function
+    return `You are ${visualFlair}${hmm}${visualFlair}!`;
+  }
+}
+
+let Calculator = {
+
+    add(x,y) { return x + y },
+    subtract(x,y) { return x - y},
+    multiply(x,y) { return x * y},
+    divide(x,y) { return x / y},
+}
+
+function actionApplyer(startingPoint, arrayOfFunction) {
+    if (arrayOfFunction.length > 0) {
+        let a = arrayOfFunction[0](startingPoint);
+        let b = arrayOfFunction[1](a);
+        return arrayOfFunction[2](b);
+    } 
+    return startingPoint;
+}
