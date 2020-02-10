2499f3de2a0ccd171156dc9b580eb3ecc20c1211
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-10 19:18:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 // Your code here
 function saturdayFun(activity="roller-skate" ){
-  return `This Saturday, I want to ${activity}`
+  return `This Saturday, I want to ${activity}!`
 }
 
 function mondayWork(activity="go to the office"){
-  return `This Monday, I will ${activity}`
+  return `This Monday, I will ${activity}.`
 }
 
 function wrapAdjective(string="*" ){
   return function(newat="special"){
-    return `You are ${string}${newat}${string}`
+    return `You are ${string}${newat}${string}!`
   }
 }
 
 let encouragingPromptFunction = wrapAdjective("!!!")
+
+
+
+let Calculator = {
+  Calculator:{},
+  add:function(){ return 1+3},
+  subtract:function(){return 1-3},
+  multiply:function(){return 1*3},
+  divide:function() {return 10/5}
+  }
+ 
+ 
+ 
+ 
+  arrayOfTransforms = [
+          function(a){ return a * 2 },
+          function(a){ return a + 1000},
+          function(a){ return a % 7 }
+        ]
+  
+  function actionApplyer(int,arr=[]){
+    return arr.reduce(arr.map(el => arr.el(int)))
+    
+  }
+
+
+
+
+
