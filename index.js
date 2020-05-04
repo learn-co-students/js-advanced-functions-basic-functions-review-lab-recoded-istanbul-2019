8944f21e6d5d13e12ff36142a078c408858a109e
refica <refia.candan@gmail.com>
2020-05-04 18:49:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(string="go to the office") {
+ return `This Monday, I will ${string}.`
+};
+
+function wrapAdjective(string = "*") {
+    return function(adjective = "special") {
+        return `You are ${string}${adjective}${string}!`
+    }
+};
+let encouragingPromptFunction = wrapAdjective("!!!")
+
+let Calculator = {
+    add(x, y) {return x + y},
+    subtract(x, y) {return x - y},
+    multiply(x, y) {return x * y},
+    divide(x, y) {return x / y},
+};
+
+function actionApplyer(int, fnArr) {
+    for (let i = 0; i < fnArr.length; i++) {
+        int = fnArr[i](int);
+    }
+    return int;
+};
