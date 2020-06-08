8d1663a1b17086d5757f464620ac543d3ffc1935
Dara <dara.dandashi@gmail.com>
2020-06-08 22:43:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(string="roller-skate"){
+    return(`This Saturday, I want to ${string}!`)
+}
+
+let mondayWork = function (string='go to the office'){
+return(`This Monday, I will ${string}.`)
+}
+
+function wrapAdjective(flair="*"){
+    return function(parameter="special"){
+        return `You are ${flair}${parameter}${flair}!`
+    }
+}
+let Calculator = {
+    add : function(x,y){
+        return x + y;
+    },
+    subtract: function (x,y){
+        return x-y;
+    },
+    multiply: function (x,y){
+        return x*y;
+    },
+    divide: function (x,y){
+        return x/y;
+    }
+}
+
+let actionApplyer = function(starting, array){
+    
+        for (let el of array) {
+            starting = el(starting)
+        }
+        return starting
+}
+
